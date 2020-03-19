896642399193c84f7077a98f27b3f32c13bd8f5b
Ameline <amelyne.ho@hotmail.fr>
2020-03-19 12:07:27 +0100

    Ajout de fonctionnalité JS changement de couleur en cliquant sur la checkbox

--- animation.js
+++ animation.js
@@ -6,3 +6,15 @@ function dropdownMenu() {
 };
 
 buttonNav.addEventListener("click", dropdownMenu);
+
+//changement fond
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+}
+
+const toggler = document.querySelector('#toggler');
+toggler.addEventListener('change', switchTheme, false);
